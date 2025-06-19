136b12e950353cbe94fb7b945ef4b47c8cb9ec0e
dichhika <dichhika123@gmail.com>
2025-06-19 12:27:22 +0545

    project part added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import photo1 from "../assets/img/Home-Img/pic2.jpg";
 import { Link } from "react-scroll";
+import { motion } from "framer-motion";
 import Button from "../components/Button";
+import html from "../assets/img/Tools-Img/html.png";
+import css from "../assets/img/Tools-Img/css.png";
+import git from "../assets/img/Tools-Img/git.png";
+import js from "../assets/img/Tools-Img/javascript.png";
+import postman from "../assets/img/Tools-Img/postman.png";
+import react from "../assets/img/Tools-Img/react.png";
+import tailwind from "../assets/img/Tools-Img/tailwind.png";
+import figma from "../assets/img/Tools-Img/figma.png";
+import bootstrap from "../assets/img/Tools-Img/bootstrap.png";
+import portgenerator from "../assets/img/Project-Img/logo.png";
+import portfolio from "../assets/img/Project-Img/Portfolio.png";
+import pla from "../assets/img/Project-Img/Pla.png";
+
 import {
   FaFacebook,
   FaLinkedin,
   FaInstagram,
   FaUser,
-  FaBirthdayCake,
   FaPhone,
   FaEnvelope,
   FaMapMarkerAlt,
@@ -15,20 +28,51 @@ import {
 } from "react-icons/fa";
 import Typewriter from "typewriter-effect";
 
-const skills = [
-  { name: "HTML", percent: 85, color: "bg-green-500" },
-  { name: "CSS", percent: 70, color: "bg-blue-500" },
-  { name: "JavaScript", percent: 65, color: "bg-yellow-500" },
-  { name: "Tailwind CSS", percent: 50, color: "bg-cyan-500" },
-  { name: "ReactJS", percent: 60, color: "bg-indigo-500" },
+const tools = [
+  { name: "HTML", image: html },
+  { name: "CSS", image: css },
+  { name: "JavaScript", image: js },
+  { name: "React", image: react },
+  { name: "Tailwind CSS", image: tailwind },
+  { name: "Postman", image: postman },
+  { name: "Git", image: git },
+  { name: "Figma", image: figma },
+  { name: "Bootstrap", image: bootstrap },
 ];
 
+const project = [
+  {
+    image: portfolio,
+    name: "Personal Portfolio",
+    description: "Built with React.js and Tailwind CSS",
+    tech1: "React.js",
+    tech2: "Tailwind CSS",
+  },
+  {
+    image: portgenerator,
+    name: "Card Generator",
+    description: "Built with HTML, CSS and JavaScripts",
+    tech1: "HTML/CSS",
+    tech2: "JavaScripts",
+  },
+  {
+    image: pla,
+    name: "Business Website",
+    description: "Built with HTML, CSS and JavaScripts",
+    tech1: "HTML/CSS",
+    tech2: "JavaScripts",
+  },
+];
 const Home = () => {
   return (
     <>
       {/* Hero Section */}
-      <div
+      <motion.div
         id="home"
+        initial={{ opacity: 0, y: 70 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
         className="bg-green-500 flex flex-col-reverse md:flex-row items-center justify-around p-6 md:p-12 lg:p-20"
       >
         <div className="p-4 text-center md:text-left">
@@ -45,7 +89,13 @@ const Home = () => {
               }}
             />
           </div>
-          <div className="flex justify-center md:justify-start gap-6 my-4">
+          <motion.div
+            initial={{ opacity: 0, y: 70 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.3 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            className="flex justify-center md:justify-start gap-6 my-4"
+          >
             <Link to="">
               <FaFacebook className="text-2xl" />
             </Link>
@@ -55,7 +105,7 @@ const Home = () => {
             <Link to="">
               <FaInstagram className="text-2xl" />
             </Link>
-          </div>
+          </motion.div>
           <Button btnStyle="p-3 border text-xl ">
             <Link to="">Download CV</Link>
           </Button>
@@ -66,10 +116,17 @@ const Home = () => {
           src={photo1}
           alt="img"
         />
-      </div>
+      </motion.div>
 
       {/* About Me Section */}
-      <div id="about" className="py-10 px-4 bg-gray-100">
+      <motion.div
+        initial={{ opacity: 0, y: 70 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        id="about"
+        className="py-10 px-4 bg-gray-100"
+      >
         <div className="flex flex-col lg:flex-row items-center justify-center gap-6 max-w-6xl mx-auto">
           <div>
             <img
@@ -95,10 +152,6 @@ const Home = () => {
                 <span>Dichhika Baral</span>
               </li>
               <li className="flex items-center gap-3">
-                <FaBirthdayCake className="text-purple-500" />
-                <span>26 August 2002</span>
-              </li>
-              <li className="flex items-center gap-3">
                 <FaPhone className="text-purple-500" />
                 <span>9816368996</span>
               </li>
@@ -122,33 +175,101 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
 
       {/* Skills Section */}
-      <div id="skills" className="max-w-4xl mx-auto p-4 sm:p-6">
-        <h2 className="text-2xl sm:text-3xl font-bold text-center text-green-500 mb-8">
-          My Skills
-        </h2>
-        {skills.map((skill, index) => (
-          <div key={index} className="mb-5">
-            <div className="flex justify-between mb-1 text-sm sm:text-lg">
-              <span className="font-medium text-gray-700">{skill.name}</span>
-              <span className="font-medium text-gray-700">
-                {skill.percent}%
-              </span>
+      <motion.section
+        className="min-h-screen flex flex-col items-center text-black  justify-center px-4"
+        id="skills"
+        initial={{ opacity: 0, y: 70 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
+        <h2 className="text-center font-bold text-5xl mb-4">My Toolbox</h2>
+        <p className="text-center  mb-10 text-2xl ">
+          Technologies and tools I work with
+        </p>
+
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-9">
+          {tools.map((tool, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                type: "tween",
+                stiffness: 100,
+                damping: 10,
+                delay: index * 0.2,
+              }}
+              className="flex flex-col items-center bg-white justify-center px-12 py-8 rounded-xl shadow-md 
+               transform transition-transform duration-300 hover:scale-110"
+            >
+              <img
+                src={tool.image}
+                alt={tool.name}
+                className="object-contain w-16 h-16 mb-3"
+              />
+              <h3 className="text-lg font-semibold">{tool.name}</h3>
+            </motion.div>
+          ))}
+        </div>
+      </motion.section>
+      <hr />
+      {/* projects */}
+      <motion.section
+        initial={{ opacity: 0, y: 70 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        className="min-h-screen flex flex-col   text-black items-center justify-center px-4 py-15"
+        id="projects"
+      >
+        <h2 className="font-bold text-5xl">My Projects</h2>
+        <p className="text-sm py-5 md:text-2xl">
+          Selected work showcasing my frontend development expertise
+        </p>
+        <div className="grid grid-cols-1 gap-3 bg-grey md:grid-cols-2 gap-12">
+          {project.map((projects, index) => (
+            <div
+              className="relative shadow-md rounded-2xl overflow-hidden h-70 w-full group"
+              key={index}
+            >
+              <img
+                src={projects.image}
+                alt={projects.name}
+                className="h-60 w-full transition-transform duration-700 group-hover:scale-105"
+              />
+              <div className="absolute bottom-0 left-0 p-4 bg-opacity-80 w-full">
+                <h3 className="text-xl font-bold text-blue-600 md:text-2x">
+                  {projects.name}
+                </h3>
+                <p className="text-green-700 text-sm md:text-lg">
+                  {projects.description}
+                </p>
+                <div className="flex ">
+                  <p className=" mr-3 py-1 px-3 rounded-full bg-gray-300">
+                    {projects.tech1}
+                  </p>
+                  <p className=" mr-3 py-1 px-3 rounded-3xl bg-gray-300">
+                    {projects.tech2}
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="w-full bg-gray-200 rounded-full h-3 sm:h-4">
-              <div
-                className={`${skill.color} h-3 sm:h-4 rounded-full`}
-                style={{ width: `${skill.percent}%` }}
-              ></div>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </motion.section>
+      <hr />
 
       {/* Contact Section */}
-      <div
+      <motion.div
+        initial={{ opacity: 0, y: 70 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
         id="contact"
         className="flex items-center justify-center bg-white px-4 py-10 text-black"
       >
@@ -193,7 +314,7 @@ const Home = () => {
             </div>
           </form>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
