770da6b3efa3e0273d66e51169dd997df1997c38
dichhika <dichhika123@gmail.com>
2025-08-12 18:18:38 +0545

    edited about section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -272,7 +272,7 @@ const Home = () => {
       <motion.div
         initial={{ opacity: 0, y: 70 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, amount: 0.3 }}
+        viewport={{ once: false, amount: 0.3 }}
         transition={{ duration: 0.8, ease: "easeOut" }}
         id="about"
         className="py-10 px-4 bg-gray-100"
